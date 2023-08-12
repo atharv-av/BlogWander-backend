55713bbc0f5327ff58e0d5e7ca112ae11364aebe
atharv-av <atharvv333@gmail.com>
2023-08-12 23:43:16 +0530

    added controllers

--- server.js
+++ server.js
@@ -6,29 +6,37 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const blogRoutes = require("./routes/blog");
+const authRoutes = require("./routes/auth");
 
 //  App
 const app = express();
 
 // Database
-mongoose.connect(process.env.DATABASE_CLOUD, {}).then(() => {console.log("DB connected")});
+mongoose.connect(process.env.DATABASE_CLOUD);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to the database");
+});
 
 // Middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
-if(process.env.NODE_ENV == "development"){
-    app.use(cors( {origin: `${process.env.CLIENT_URL}` }));
+if (process.env.NODE_ENV == "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
 app.use(blogRoutes);
+app.use(authRoutes);
 
 // Routes
 app.get("/api", (req, res) => {
-    res.json({ time: Date().toString() });
+  res.json({ time: Date().toString() });
 });
 
 //  Port
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`app is running on port ${port}`);
+  console.log(`App is running on port ${port}`);
 });
